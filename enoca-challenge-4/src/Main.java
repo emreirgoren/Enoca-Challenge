b4c3cfb926516c89b6350cba31f1de9da93f3811
emreirgoren <emreirgoren@outlook.com>
2025-01-25 15:37:11 +0300

    Added screenshot and postman collection

--- enoca-challenge-4/src/Main.java
+++ enoca-challenge-4/src/Main.java
@@ -7,19 +7,24 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-
+        // Orjinal liste oluşturma
         List<Integer> orginal = new ArrayList<>();
 
+        //Random sınıfı
         Random random = new Random();
 
+        //orjinal listeyi random sayılarla doldurma
         for (int i = 0; i < 100; i++) {
             orginal.add(random.nextInt(100));
         }
 
+        //Kopya liste olusturma
         List<Integer> copy = new ArrayList<>(orginal);
 
+        //Silinecek sayıyı olusturma
         Integer randomNumber = Integer.valueOf(random.nextInt(100));
 
+        //Silincek sayı ile eşlenşen liste elamanını bulup silme
         if(copy.stream().anyMatch(number -> number == randomNumber)){
             copy.remove(randomNumber);
         }
@@ -28,6 +33,7 @@ public class Main {
 
     }
 
+    //Silinen sayıyı bulan metot
     public static String getRemoveNumber(List<Integer> orginalList,List<Integer> copyList) throws Exception {
 
         for(Integer num : orginalList){
